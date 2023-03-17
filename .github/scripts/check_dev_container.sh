1bfe0b1e64ffc2513a2c91225dcc9a1061ba28cf
Steen Kroyer <stek@via.dk>
2023-03-17 09:54:42 +0000

    Added some debug echoes

--- .github/scripts/check_dev_container.sh
+++ .github/scripts/check_dev_container.sh
@@ -2,7 +2,7 @@
 
 # Change to .decontainer folder and calculate Dockerfile hash
 cd "${GITHUB_WORKSPACE}"/.devcontainer || exit 1
-IMG_NAME="ghcr.io/dafessor/devops-demo-img"
+IMG_NAME="ghcr.io/dafessor/devops-demo-img:latest"
 DOCKER_HASH=$(sha1sum Dockerfile 2>&1 || true)
 # Get info on any existing image and try to match hashes
 IMAGE_UP2DATE=$( (docker inspect ${IMG_NAME} 2>&1 || true) | (grep -c "${DOCKER_HASH}" || true) )
@@ -10,8 +10,11 @@ IMAGE_UP2DATE=$( (docker inspect ${IMG_NAME} 2>&1 || true) | (grep -c "${DOCKER_
 
 # If hashes don't (or don exist), build and push image
 if [ "${IMAGE_UP2DATE}" != "1" ]; then
+    echo "Rebuilding image, deleting any old stuff ...."
     docker image rm "${IMG_NAME}" || true
-    docker build -t "${IMG_NAME}":latest --label org.opencontainers.image.description="${DOCKER_HASH}" .
+    echo "Doing the actual build ...."
+    docker build -t "${IMG_NAME}" --label org.opencontainers.image.description="${DOCKER_HASH}" .
+    echo "Pushing new image ...."
     docker push "${IMG_NAME}"
 fi
 
