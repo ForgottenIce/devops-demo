ae52e21857a0a51ab85369b9be1129e948ed42d0
Steen Kroyer <stek@via.dk>
2023-03-17 09:38:16 +0000

    Fixed shellcheck issues in script

--- .github/scripts/check_dev_container.sh
+++ .github/scripts/check_dev_container.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -l
 
 # Change to .decontainer folder and calculate Dockerfile hash
-cd ${GITHUB_WORKSPACE}/.devcontainer
+cd "${GITHUB_WORKSPACE}"/.devcontainer || exit 1
 IMG_NAME="ghcr.io/dafessor/devops-demo-img"
 DOCKER_HASH=$(sha1sum Dockerfile 2>&1 || true)
 # Get info on any existing image and try to match hashes
-IMAGE_UP2DATE=$((docker inspect ${IMG_NAME} 2>&1 || true) | (grep -c ${DOCKER_HASH} || true))
+IMAGE_UP2DATE=$( (docker inspect ${IMG_NAME} 2>&1 || true) | (grep -c "${DOCKER_HASH}" || true) )
 
 
 # If hashes don't (or don exist), build and push image
 if [ "${IMAGE_UP2DATE}" != "1" ]; then
     docker image rm "${IMG_NAME}" || true
-    docker build -t "${IMG_NAME}":latest --label org.opencontainers.image.description=${DOCKER_HASH} .
+    docker build -t "${IMG_NAME}":latest --label org.opencontainers.image.description="${DOCKER_HASH}" .
     docker push "${IMG_NAME}"
 fi
 
