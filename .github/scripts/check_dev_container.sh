6504440880847b392924cff8a46f4768efc14e04
Steen Kroyer <stek@via.dk>
2023-03-18 11:55:22 +0000

    Use GitHup REST api to check image

--- .github/scripts/check_dev_container.sh
+++ .github/scripts/check_dev_container.sh
@@ -1,15 +1,27 @@
 #!/bin/sh -l
 
+if [ ! $# -eq 1 ]; then
+    exit 1
+fi
+
+GITHUB_TOKEN=$1
+
+# Constants
+IMG_NAME="ghcr.io/dafessor/devops-demo/devops-demo-img:latest"
+IMG_NAME_URL_ENC="devops-demo%2Fdevops-demo-img"
+
 # Change to .decontainer folder and calculate Dockerfile hash
 cd "${GITHUB_WORKSPACE}"/.devcontainer || exit 1
-IMG_NAME="ghcr.io/dafessor/devops-demo/devops-demo-img:latest"
 DOCKER_HASH=$(sha1sum Dockerfile 2>&1 || true)
-# Get info on any existing image and try to match hashes
-IMAGE_UP2DATE=$( (docker inspect ${IMG_NAME} 2>&1 || true) | (grep -c "${DOCKER_HASH}" || true) )
 
+# Get info on any existing image and try to match hashes
+IMG_UP2DATE=$(curl -s -L -H "Accept: application/vnd.github+json" \
+                   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+                   -H "X-GitHub-Api-Version: 2022-11-28" \
+                   "https://api.github.com/user/packages/container/${IMG_NAME_URL_ENC}/versions" | grep -c "${DOCKER_HASH}")
 
 # If hashes don't (or don exist), build and push image
-if [ "${IMAGE_UP2DATE}" != "1" ]; then
+if [ "${IMG_UP2DATE}" -lt  1 ]; then
     echo "Rebuilding image, deleting any old stuff ...."
     docker image rm "${IMG_NAME}" || true
     echo "Doing the actual build ...."
