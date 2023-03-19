624f8a7e431b133462dd48b0c891e195fae76e97
Steen Kroyer <stek@via.dk>
2023-03-19 13:03:58 +0000

    More case fiddling

--- .github/scripts/check_dev_container.sh
+++ .github/scripts/check_dev_container.sh
@@ -6,12 +6,11 @@ if [ ! $# -eq 3 ]; then
 fi
 
 REPO_NAME=$1
-IMAGE_NAME=$2
+IMG_NAME=$2
 GITHUB_TOKEN=$3
 
 # Constants
-IMG_NAME="$(echo "${IMAGE_NAME}" | tr '[:upper:]' '[:lower:]')"
-IMG_NAME_URL_ENC=$(echo "${REPO_NAME}" | tr '[:upper:]' '[:lower:]' | sed s/\\//%2F/)
+REPO_NAME_URL_ENC=$(echo "${REPO_NAME}" | tr '[:upper:]' '[:lower:]' | sed s/\\//%2F/)
 
 # Change to .decontainer folder and calculate Dockerfile hash
 cd "${GITHUB_WORKSPACE}"/.devcontainer || exit 1
@@ -21,7 +20,7 @@ DOCKER_HASH=$(sha1sum Dockerfile 2>&1 || true)
 IMG_UP2DATE=$(curl -s -L -H "Accept: application/vnd.github+json" \
                    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
                    -H "X-GitHub-Api-Version: 2022-11-28" \
-                   "https://api.github.com/user/packages/container/${IMG_NAME_URL_ENC}/versions" | grep -c "${DOCKER_HASH}")
+                   "https://api.github.com/user/packages/container/${REPO_NAME_URL_ENC}/versions" | grep -c "${DOCKER_HASH}")
 
 # If hashes don't (or don exist), build and push image
 if [ "${IMG_UP2DATE}" -lt  1 ]; then
